11fa9bd7056704cc9faaf27a1c63e2ec59e52524
Priyadharshini Govindarasu <31530607+priyadharshinigovind@users.noreply.github.com>
2018-02-13 14:54:03 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+  float l,b,area;
+  printf("\nEnter length and breadth"); 
+  scanf("%f%f",&l,&b);
+  area=l*b;
+  printf("%0.5f",area);
+
+}
